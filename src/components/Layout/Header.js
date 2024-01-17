1ac4e8da57184ff091e82855ebf58460ac3d6aa7
ankitsaini-github <ankit121saini@gmail.com>
2024-01-17 09:42:55 +0530

    headerBanner added to header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import classes from './Header.module.css'
+import React from "react";
+import classes from "./Header.module.css";
+import headerBanner from '../../assets/headerBanner.jpg';
 
 function Header() {
   return (
-    <div className={classes.headerContainer}>
+    <>
+      <header className={classes.headerContainer}>
         <div className={classes.logo}>ReactMeals</div>
-        <div className={classes.cartValue}>
-            <h6>Your Cart</h6>
-            <div className={classes.CartCount}>0</div>
-        </div>
-    </div>
-  )
+        <button className={classes.cartValue}>
+          <h6>Your Cart</h6>
+          <div className={classes.CartCount}>0</div>
+        </button>
+      </header>
+      <div className={classes.headerBanner}>
+        <img src={headerBanner}/>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
