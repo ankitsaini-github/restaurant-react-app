3a5a71c289f43b88a49ecd3d310f5ece1ebc90dd
ankitsaini-github <ankit121saini@gmail.com>
2024-01-17 17:16:15 +0530

    Making the meal Items visible

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,10 +1,11 @@
 import {Fragment} from 'react'
 import MealsSummary from './MealsSummary'
-
+import AvailableMeal from './AvailableMeal'
 function Meals() {
   return (
     <Fragment>
         <MealsSummary/>
+        <AvailableMeal/>
     </Fragment>
   )
 }
