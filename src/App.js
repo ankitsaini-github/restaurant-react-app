45149b531723ffea00dcdf9452257cf75739a796
ankitsaini-github <ankit121saini@gmail.com>
2024-01-18 15:44:20 +0530

    Cart open and close

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import Header from "./components/Layout/Header";
 import './App.css'
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import { useState } from "react";
 
 function App() {
+  const [showCart,setShowCart]=useState(false);
+
+  const ShowCartHandler=()=>{
+    setShowCart(!showCart)
+  }
   return (
     <div className="app">
-      <Cart/>
-      <Header/>
+      {showCart && <Cart onClose={ShowCartHandler}/>}
+      <Header onOpen={ShowCartHandler}/>
       <main>
         <Meals/>
       </main>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import classes from "./Header.module.css";
 import headerBanner from '../../assets/headerBanner.jpg';
 import CartIcon from "../Cart/CartIcon";
 
-function Header() {
+function Header(props) {
   return (
     <>
       <header className={classes.headerContainer}>
         <div className={classes.logo}>ReactMeals</div>
-        <button className={classes.cartButton}>
+        <button className={classes.cartButton} onClick={props.onOpen}>
           <span className={classes.icon}><CartIcon/></span>
           <span>Your Cart</span>
           <span className={classes.badge}>0</span>
