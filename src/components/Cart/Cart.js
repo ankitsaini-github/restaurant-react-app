913f123a360f45204e7a0b5ec190cce65d9d6973
ankitsaini-github <ankit121saini@gmail.com>
2024-01-19 11:17:52 +0530

    cart item add and remove function

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,32 +1,53 @@
-import Modal from '../UI/Modal';
-import classes from './Cart.module.css'
-import CartContext from '../../store/Cart-context';
-import { useContext } from 'react';
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/Cart-context";
+import { useContext } from "react";
+import CartItem from "./CartItem";
 
 function Cart(props) {
-  const ctx=useContext(CartContext);
+  const ctx = useContext(CartContext);
   const price = `$${ctx.totalAmount.toFixed(2)}`;
-    const cartItems = (
-        <ul className={classes['cart-items']}>
-          {ctx.items.map((item) => (
-            <li key={`c_${item.id}`}>{item.name} - price: ${item.price} - qty: {item.amount}</li>
-          ))}
-        </ul>
-      );
-    
-      return (
-        <Modal onBackdropClick={props.onClose}>
-          {cartItems}
-          <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>{price}</span>
-          </div>
-          <div className={classes.actions}>
-            <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-            <button className={classes.button}>Order</button>
-          </div>
-        </Modal>
-      );
+  const cartEmpty = ctx.items.length < 1;
+
+  const cartItemRemoveHandler = (id) => {
+    ctx.removeItem(id)
+  };
+
+  const cartItemAddHandler = (item) => {
+    ctx.addItem({...item,amount:1})
+  };
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {ctx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onRemove={cartItemRemoveHandler.bind(null,item.id)}
+          onAdd={cartItemAddHandler.bind(null,item)}
+        />
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onBackdropClick={props.onClose}>
+      {cartEmpty && <p>Cart is Empty !!!</p>}
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{price}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {!cartEmpty && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
 }
 
-export default Cart
+export default Cart;
